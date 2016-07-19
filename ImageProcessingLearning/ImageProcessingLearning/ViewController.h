0be03ae1b9ed210389fd3205df52109fd80e45da
IOS <ios@jykj.local>
2016-07-19 10:27:53 +0800

    Initial commit

---
+++ ImageProcessingLearning/ImageProcessingLearning/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageProcessingLearning
+//
+//  Created by IOS on 16/7/19.
+//  Copyright © 2016年 IOS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ImageProcessingLearning/ImageProcessingLearning/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ImageProcessingLearning
+//
+//  Created by IOS on 16/7/19.
+//  Copyright © 2016年 IOS. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
